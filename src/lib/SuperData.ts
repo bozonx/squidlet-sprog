ced015e296989bb4b8b4744abfb29fd9a2b5f2d0
Ivan <ipkozyrin@gmail.com>
2023-06-01 13:33:04 -0300

    add

--- src/lib/SuperData.ts
+++ src/lib/SuperData.ts
@@ -2,6 +2,15 @@ import {SuperValueBase} from './SuperValueBase.js';
 import {AllTypes} from '../types/valueTypes.js';
 
 
-export class SuperData<T = Record<string | number, AllTypes>> extends SuperValueBase<T> {
+// TODO: он может содержать ключи как string так и number
+// TODO: он может работать как массив и объект одновременно
+// TODO: можно сортировать ключи, reverse, pop, etc
+// TODO: добавление нового элемента это push
+// TODO: тип, ro и тд ставится сразу на весь объект как в массиве
+// TODO: типов может быть несколько
+// TODO: required не будет, поэтому возможен undefined
+
+
+export class SuperData<T = any> extends SuperValueBase<Record<string| number, T>> {
   readonly isData = true
 }
