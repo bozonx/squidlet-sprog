5159f719da8cdaa2815add6e6065555ed4b9015e
Ivan <ipkozyrin@gmail.com>
2023-06-05 14:06:27 -0300

    change

--- src/lib/SuperFunc.ts
+++ src/lib/SuperFunc.ts
@@ -13,7 +13,7 @@ const SUPER_RETURN = 'superReturn'
 
 // TODO: можно по каждому prop добавить combined в scope как алиас
 // TODO: если в prop есть супер значение то им должно быть проставлено readonly
-// TODO: может добавить событие вызова ф-и???
+// TODO: может добавить событие вызова ф-и или лучше middleware???
 
 
 export class SuperFunc<T = Record<string, AllTypes>> extends SuperBase {
