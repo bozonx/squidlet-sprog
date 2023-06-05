5cfa97c1146cac0e3678f253ae052f8f3b210ca5
Ivan <ipkozyrin@gmail.com>
2023-06-05 18:22:05 -0300

    test

---
+++ test/lang/deepValue.spec.js
@@ -0,0 +1,28 @@
+import {newScope} from "../../src/index.js";
+import {getValue} from "../../src/lang/deepValue.js";
+
+
+describe('deepValue', () => {
+  it('getValue, setValue, deleteValue deeply', async () => {
+    const scope = newScope({v1: {}})
+
+    await scope.$run({
+      $exp: 'setValue',
+      path: 'v1.v2',
+      value: 5,
+    })
+
+    assert.deepEqual(scope, {v1: {v2: 5}})
+    assert.equal(await scope.$run({
+      $exp: 'getValue',
+      path: 'v1.v2',
+    }), 5)
+
+    await scope.$run({
+      $exp: 'deleteValue',
+      path: 'v1.v2',
+    })
+
+    assert.deepEqual(scope, {v1: {}})
+  })
+})
